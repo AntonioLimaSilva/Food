77a81f8a9bcd4106b30426bb344fd0e68073ac3a
Luciano Lima <luclimasilva@hotmail.com>
2019-12-07 18:08:20 -0300

    add

--- src/main/java/br/com/luciano/food/FoodApplication.java
+++ src/main/java/br/com/luciano/food/FoodApplication.java
@@ -2,13 +2,12 @@ package br.com.luciano.food;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class FoodApplication {
 
     public static void main(String[] args) {
-        ApplicationContext context = SpringApplication.run(FoodApplication.class, args);
+        SpringApplication.run(FoodApplication.class, args);
     }
 
 }
